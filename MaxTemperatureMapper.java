688753485275f9bea775f11bc8d5e0d640478b55
wolvesled <wolvesled@gmail.com>
2013-08-27 21:21:37 +0800

    Update MaxTemperatureMapper.java

--- MaxTemperatureMapper.java
+++ MaxTemperatureMapper.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Mapper;
 
 public class MaxTemeratureMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
 
