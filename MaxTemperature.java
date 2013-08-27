8f669c559dd789dff96d1ee6f64b93419f89f3ce
wolvesled <wolvesled@gmail.com>
2013-08-27 21:18:48 +0800

    Update MaxTemperature.java

--- MaxTemperature.java
+++ MaxTemperature.java
@@ -7,25 +7,25 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class MaxTemperature {
 
-  public static void main(String[] args) throws Exception {
-    if (args.length != 2) {
-      System.err.println("Usage: MaxTemperature <input path> <output path>");
-      System.exit(-1);
-    }
+	public static void main(String[] args) throws Exception {
+		if (args.length != 2) {
+			System.err.println("Usage: MaxTemperature <input path> <output path>");
+			System.exit(-1);
+		}
+		
+		Job job = new Job();
+		job.setJarByClass(MaxTemperature.class);
+		job.setJobName("Max temperature");
+		
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
     
-    Job job = new Job();
-    job.setJarByClass(MaxTemperature.class);
-    job.setJobName("Max temperature");
+		job.setMapperClass(MaxTemperatureMapper.class);
+		job.setReducerClass(MaxTemperatureReducer.class);
+		
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
     
-    FileInputFormat.addInputPath(job, new Path(args[0]));
-    FileOutputFormat.setOutputPath(job, new Path(args[1]));
-    
-    job.setMapperClass(MaxTemperatureMapper.class);
-    job.setReducerClass(MaxTemperatureReducer.class);
-    
-    job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(IntWritable.class);
-    
-    System.exit(job.waitForCompletion(true) ? 0 : 1);
-  }
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
 }
